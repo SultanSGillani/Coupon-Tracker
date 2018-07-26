8e530af6721197f8361bd2c075d547ec56131562
saltman <saltman@jumpcut.to>
2018-07-26 18:01:52 -0400

    add in some models.

---
+++ coupon_tracker/coupons/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.dispatch import Signal
+
+COUPON_TYPES = (
+	('monetary', 'Money based coupon'),
+	('percentage', 'Percentage discount'),
+	('virtual_currency', 'Virtual currency'),
+)
+
+redeem_done = Signal(providing_args=["coupon"])
+
+
+class CouponManager(models.Manager):
+	def create_coupon(self, type, value, users=[], valid_until=None):
+		coupon = self.create(
+			value=value,
+			type=type,
+			valid_until=valid_until,
+		)
+		coupon = Coupon.objects.create_coupon(type, value, users, valid_until)
+		if not isinstance(users, list):
+			users = [users]
+		for user in users:
+			if user:
+				CouponUser(user=user, coupon=coupon).save()
+		return coupon
+
+	def create_coupons(self, quantity, type, value, valid_until=None):
+		coupons = []
+		for i in range(quantity):
+			coupons.append(self.create_coupon(type, value, valid_until))
+		return coupons
+
+	def used(self):
+		return self.exclude(users__redeemed_at__isnull=True)
+
+	def unused(self):
+		return self.filter(users__redeemed_at__isnull=True)
+
+	def expired(self):
+		return self.filter(valid_until__lt=timezone.now())
+
+
+# Create your models here.
+class Coupon(models.Model):
+	price = models.DecimalField(max_digits=8, decimal_places=2)
+	discount = models.DecimalField(max_digits=6, decimal_places=2)
+	store = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+	type = models.CharField(max_length=20, choices=COUPON_TYPES)
+	valid_until = models.DateTimeField(blank=True, null=True)
+	objects = CouponManager()
+
+	class Meta:
+		ordering = ['created_at']
+
+	def expired(self):
+		return self.valid_until is not None and self.valid_until < timezone.now()
+
+	@property
+	def redeemed_at(self):
+		try:
+			return self.users.filter(redeemed_at__isnull=False).order_by('redeemed_at').last().redeemed_at
+		except self.users.through.DoesNotExist:
+			return None
+
+	def redeem(self, user=None):
+		try:
+			coupon_user = self.users.get(user=user)
+		except CouponUser.DoesNotExist:
+			try:  # silently fix unbouned or nulled coupon users
+				coupon_user = self.users.get(user__isnull=True)
+				coupon_user.user = user
+			except CouponUser.DoesNotExist:
+				coupon_user = CouponUser(coupon=self, user=user)
+		coupon_user.redeemed_at = timezone.now()
+		coupon_user.save()
+		redeem_done.send(sender=self.__class__, coupon=self)
+
+
+class CouponUser(models.Model):
+	coupon = models.ForeignKey(Coupon, related_name='users', on_delete=models.CASCADE)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
+	redeemed_at = models.DateTimeField(blank=True, null=True)
+
+	class Meta:
+		unique_together = ('coupon', 'user')
+
+	def __str__(self):
+		return str(self.user)
